ba89415db428153813e49024dbd0854042c6fc2f
Chris Carrick <36552610+cjcarrick@users.noreply.github.com>
2023-05-28 14:45:10 -0700

    Add missing transforms.cpp file

---
+++ lib/transforms.cpp
@@ -0,0 +1,55 @@
+#include "./transforms.h"
+#include "./util.h"
+#include <regex>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+namespace transforms
+{
+
+OPERATION parse_delimiter(string delimiter)
+{
+    if (delimiter == "*") return MULTIPLY;
+    if (delimiter == "-") return SUBTRACT;
+    if (delimiter == "+") return ADD;
+    if (delimiter == "=") return SET;
+    return NUL_OPERATION;
+}
+
+CHANNEL parse_channel(string channel)
+{
+    if (channel == "rgb_r") return RGB_R;
+    if (channel == "rgb_g") return RGB_G;
+    if (channel == "rgb_b") return RGB_B;
+    if (channel == "hsl_h") return HSL_H;
+    if (channel == "hsl_s") return HSL_S;
+    if (channel == "hsl_l") return HSL_L;
+    if (channel == "hsv_h") return HSV_H;
+    if (channel == "hsv_s") return HSV_S;
+    if (channel == "hsv_v") return HSV_V;
+    return NUL_CHANNEL;
+}
+
+vector<ColorTransform> from_str(string s)
+{
+    auto xf_strs = split(s, std::regex("\\s*,\\s*"));
+    std::regex regexp(
+        "([a-z_]+)([+-=*])([0-9.]+)", std::regex_constants::icase
+    );
+    std::smatch sm;
+
+    vector<ColorTransform> xfs;
+    ColorTransform xf;
+    for (auto &xf_str : xf_strs) {
+        if (!std::regex_search(xf_str, sm, regexp)) continue;
+        xf.channel = parse_channel(sm[1].str()),
+        xf.operation = parse_delimiter(sm[2].str()),
+        xf.value = (int)parse_num(sm[3].str());
+        xfs.push_back(xf);
+    }
+    return xfs;
+}
+} // namespace transforms
